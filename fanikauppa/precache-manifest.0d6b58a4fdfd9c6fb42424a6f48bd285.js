9895a183515cac2da02fbe2a7deb3529f3c2da9c
Tiia Hakkarainen <tiia.hakkarainen2@edu.tampere.fi>
2020-12-17 08:55:19 +0200

    17.12.20

---
+++ fanikauppa/precache-manifest.0d6b58a4fdfd9c6fb42424a6f48bd285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c1c8bcabbd2374063459b10765cb89",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "71f29fc696b8da1af823",
+    "url": "/fanikauppa/static/css/main.c5d28172.chunk.css"
+  },
+  {
+    "revision": "025761d45b07d8dac28b",
+    "url": "/fanikauppa/static/js/2.2d250f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.2d250f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f29fc696b8da1af823",
+    "url": "/fanikauppa/static/js/main.e6e8c79d.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  }
+]);
